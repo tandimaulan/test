b4377fb6e1eaa9573a1fca94065c229fba091bc8
tandimaulan <79802912+tandimaulan@users.noreply.github.com>
2024-05-14 00:08:39 +0700

    Add files via upload

---
+++ technical.test/src/test/java/com/technical/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
